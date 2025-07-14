8752007e5d20d88bbf4a5da7e408a40fc6f0b371
Print Mania <meniaprint@gmail.com>
2025-07-14 10:37:37 +0000

    bees i want on front of products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -35,7 +35,7 @@ export default function ProductsPage() {
           {categories.map((category) => (
             <TabsContent key={category.id} value={category.id} className="relative">
                {category.id === 'honey' && (
-                  <div className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none -z-10">
+                  <div className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none">
                     <Bee style={{ animation: 'fly-1 15s linear infinite', top: '10%', left: '0', animationDelay: '0s' }} />
                     <Bee style={{ animation: 'fly-2 18s linear infinite', top: '30%', left: '0', animationDelay: '2s' }} />
                     <Bee style={{ animation: 'fly-3 20s linear infinite', top: '50%', left: '0', animationDelay: '5s' }} />
@@ -57,3 +57,4 @@ export default function ProductsPage() {
     </div>
   );
 }
+
