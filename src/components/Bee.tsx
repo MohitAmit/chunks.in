a9f0a424fe4d4680af855c532f2b1c5f22f76c9a
Print Mania <meniaprint@gmail.com>
2025-07-14 10:57:25 +0000

    i want bees like this

--- src/components/Bee.tsx
+++ src/components/Bee.tsx
@@ -8,23 +8,27 @@ export function Bee({ className, ...props }: React.SVGProps<SVGSVGElement>) {
             className={cn("absolute w-12 h-12", className)}
             {...props}
         >
-            <g transform="translate(20 20) rotate(-30)">
-                {/* Left Wing */}
-                <path d="M45 30 C 25 10, 25 50, 45 50 Z" fill="#FDD85D" />
-                {/* Right Wing */}
-                <path d="M55 30 C 75 10, 75 50, 55 50 Z" fill="#FDD85D" />
-                
+            <g transform="translate(50,50) rotate(-15)">
+                {/* Wings */}
+                <path d="M 5, -15 C -20, -40, -20, 0, 5, -5" fill="#e3f2fd" opacity="0.9" />
+                <path d="M -5, -15 C 20, -40, 20, 0, -5, -5" fill="#bbdefb" opacity="0.9" />
+
                 {/* Body */}
-                <path d="M50 35 C 70 35, 80 50, 65 65 C 50 80, 30 80, 25 65 C 10 50, 30 35, 50 35 Z" fill="#FFFFFF"/>
+                <ellipse cx="0" cy="0" rx="25" ry="20" fill="#ffc107" />
                 
                 {/* Stripes */}
-                <path d="M50 42 C 65 42, 70 50, 60 55 C 50 60, 35 60, 30 55 C 20 50, 35 42, 50 42 Z" fill="#000000" />
-                <path d="M50 58 C 62 58, 65 65, 58 70 C 50 75, 40 75, 35 70 C 30 65, 38 58, 50 58 Z" fill="#000000" />
-                <path d="M50 35 C 70 35, 80 50, 65 65 C 50 80, 30 80, 25 65 C 10 50, 30 35, 50 35 Z" fill="none" stroke="#000000" strokeWidth="2"/>
+                <path d="M -23 5 C -20 15, 20 15, 23 5 Q 24 -5 -24 -5 Z" fill="#212121" />
+                <path d="M -15 -13 C -12 -5, 12 -5, 15 -13 Q 12 -18 -12 -18 Z" fill="#212121" />
+                
+                {/* Face */}
+                <circle cx="-7" cy="-2" r="2" fill="#212121" />
+                <circle cx="7" cy="-2" r="2" fill="#212121" />
 
-                {/* Eyes */}
-                <circle cx="45" cy="38" r="1.5" fill="#000000" />
-                <circle cx="55" cy="38" r="1.5" fill="#000000" />
+                {/* Antennae */}
+                <path d="M -10 -18 Q -18 -25 -15 -30" stroke="#212121" strokeWidth="2" fill="none" />
+                <path d="M 10 -18 Q 18 -25 15 -30" stroke="#212121" strokeWidth="2" fill="none" />
+                <circle cx="-15" cy="-30" r="2.5" fill="#212121" />
+                <circle cx="15" cy="-30" r="2.5" fill="#212121" />
             </g>
         </svg>
     )
