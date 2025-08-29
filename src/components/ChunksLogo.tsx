b32b99ff7ca3fe5010004fcd87ce0b0007995421
Print Mania <meniaprint@gmail.com>
2025-08-29 16:19:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ChunksLogo>
    
    with

--- src/components/ChunksLogo.tsx
+++ src/components/ChunksLogo.tsx
@@ -1,28 +1,15 @@
-
 'use client';
+import Image from 'next/image';
 
 export const ChunksLogo = () => {
-    return (
-      <svg
-        width="150"
-        height="40"
-        viewBox="0 0 160 42"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="object-contain"
-      >
-        <style>
-          {`
-            @import url('https://fonts.googleapis.com/css2?family=Alegreya:wght@700&display=swap');
-            .logo-text {
-              font-family: 'Alegreya', serif;
-              font-size: 32px;
-              font-weight: 700;
-              fill: hsl(var(--foreground));
-            }
-          `}
-        </style>
-        <text x="0" y="30" className="logo-text">Chunks</text>
-      </svg>
-    );
+  return (
+    <Image
+      src="https://i.ibb.co/mFVXm9M5/Chat-GPT-Image-Aug-29-2025-09-47-25-PM.png"
+      alt="Chunks Logo"
+      width={140}
+      height={40}
+      className="object-contain"
+      priority
+    />
+  );
 };
