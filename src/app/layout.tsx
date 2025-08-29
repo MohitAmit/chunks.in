5850324819502cdb0dfcb4d5f35acc2a71152da0
Print Mania <meniaprint@gmail.com>
2025-08-29 16:48:00 +0000

    by default it should be in light mode only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,7 @@ export default function RootLayout({
       <body className={cn('font-body antialiased')}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="light"
             disableTransitionOnChange
         >
           <CartProvider>
