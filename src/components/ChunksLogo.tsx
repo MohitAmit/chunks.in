461fb4d82675d19cca9c11c3caa202027e39fc97
Print Mania <meniaprint@gmail.com>
2025-08-29 16:12:50 +0000

    remove the background of the logo

--- src/components/ChunksLogo.tsx
+++ src/components/ChunksLogo.tsx
@@ -1,16 +1,28 @@
-'use client';
 
-import Image from 'next/image';
+'use client';
 
 export const ChunksLogo = () => {
     return (
-      <Image
-        src="https://i.ibb.co/ynCgxZ9h/Screenshot-2025-07-14-at-8-04-49-PM.png"
-        alt="Chunks Logo"
-        width={150}
-        height={40}
+      <svg
+        width="150"
+        height="40"
+        viewBox="0 0 160 42"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
         className="object-contain"
-        priority
-      />
+      >
+        <style>
+          {`
+            @import url('https://fonts.googleapis.com/css2?family=Alegreya:wght@700&display=swap');
+            .logo-text {
+              font-family: 'Alegreya', serif;
+              font-size: 32px;
+              font-weight: 700;
+              fill: hsl(var(--foreground));
+            }
+          `}
+        </style>
+        <text x="0" y="30" className="logo-text">Chunks</text>
+      </svg>
     );
 };
