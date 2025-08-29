ea048224f1dee0b60df9a4b0f7f31570af0d3d9d
Print Mania <meniaprint@gmail.com>
2025-08-29 19:31:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ import { ChunksLogo } from './ChunksLogo'
 import { ThemeToggle } from './ThemeToggle'
 
 const navLinks = [
-  { href: '/products', label: <>चख लो</> },
+  { href: '/products', label: 'just try' },
   { href: '/about', label: <>My Kahani</> },
   { href: '/contact', label: <>बात-चीत</> },
   { href: '/blog', label: <>अनोखा ज्ञान</> },
